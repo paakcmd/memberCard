a64c2eb90b1bd1f4a4585fdeb12b27323ad387da
paakcmd <paakcmd@users.noreply.github.com>
2018-08-05 15:46:02 +0700

    auth flow

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/Users');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const authRoutes = require('./routes/authRoutes');
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+
+
+
+
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' })
-})
-
 app.listen(PORT);
